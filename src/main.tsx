3ada9cb11ba34b1f01235392c719e7bbad1f14f5
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-11-13 23:28:58 +0100

    Wrap application in DataProvider and Layout components for improved state management and structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
-import School from './pages/School'
-import Timestamp from './pages/Timestamp'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import School from './pages/School';
+import Timestamp from './pages/Timestamp';
+import { DataProvider } from './contexts/DataContext';
+import Layout from './components/Layout';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path='school/:id' element={<School />} />
-        <Route path='timestamp/:id' element={<Timestamp />} />
-      </Routes>
-    </BrowserRouter>
+    <DataProvider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path='school/:id' element={<School />} />
+            <Route path='timestamp/:id' element={<Timestamp />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </DataProvider>
   </StrictMode>,
 )
