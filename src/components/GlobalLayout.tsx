4ac3e76c08d76dbe6a92657e86e756561be22d55
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-11-14 23:45:29 +0100

    fix: Improve loading state UI in GlobalLayout component

--- src/components/GlobalLayout.tsx
+++ src/components/GlobalLayout.tsx
@@ -5,7 +5,11 @@ export default function GlobalLayout({ children }: { children: React.ReactNode }
   const data = useData();
 
   if (data.loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className='flex justify-center items-center min-h-screen'>
+        <span className="loading loading-spinner loading-xl"></span>
+      </div>
+    );
   }
   else if (data.error) {
     return <div>Error: {data.error.message}</div>;
