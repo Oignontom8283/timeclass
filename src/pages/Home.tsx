9405c09d5c85ae956c2f9560ff01afc974cf0f81
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-11-15 17:15:03 +0100

    fix: Update Home component layout and utilize addressFormatter for school addresses

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,24 @@
+import { useData } from "../contexts/DataContext";
+import { addressFormatter } from "../utils/formatters";
 
 export default function Home() {
+
+  const data = useData();
+
   return (
     <div>
-      <h1>TimeClass</h1>
+      <div>
+        <h1>TimeClass</h1>
+        <p>Welcome to TimeClass !<br/> Schedule for your school.</p>
+      </div>
+      <ul>
+        {data.schools.map(school => (
+          <li key={school.id}>
+            <h2>{school.name.original}</h2>
+            <p>{addressFormatter(school.address)}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
