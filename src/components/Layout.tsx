4d6e1c6d981b81e236c72fc4fa4eacc9aae1d9b6
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-11-16 00:34:05 +0100

    fix: Restore header layout in Layout component for consistent styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom';
 export default function Layout() {
   return (
     <div className="min-h-screen">
-      <header className="">
-        <div></div>
+      <header className="w-full h-12 flex items-center justify-center border-b border-gray-300 bg-zinc-50">
+        <div className='font-mono text-2xl font-bold'>TimeClass</div>
       </header>
 
       <main className="">
