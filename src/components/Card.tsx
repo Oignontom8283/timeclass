c316c40eeb94f8ca4c4ab0416dfc6e159671b069
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-11-17 00:40:54 +0100

    feat: Enhance Card component to accept props for title, subtitle, and description

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,10 @@
 
-export default function Card() {
+export default function Card({title, titleSecondary, description}: { title: string; titleSecondary?: string; description: string }) {
   return (
-    <div>
-      Card Component
+    <div className="flex flex-col bg-zinc-50 p-2 border border-gray-300 rounded-lg shadow-md">
+      <span className="font-bold">{title}</span>
+      {titleSecondary && <span className="text-sm text-gray-600">{titleSecondary}</span>}
+      <p className="italic text-sm mt-1">{description}</p>
     </div>
   )
 }
