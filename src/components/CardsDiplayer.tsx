d8d409e471d48b38dbb203522094a38bdc202a25
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-11-17 00:40:57 +0100

    feat: Implement CardsDiplayer component to display school cards with links and formatted addresses

--- src/components/CardsDiplayer.tsx
+++ src/components/CardsDiplayer.tsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
+import { useData } from "../contexts/DataContext"
+import { addressFormatter } from "../utils/formatters";
+import Card from "./Card";
 
 export default function CardsDiplayer() {
+
+  const data = useData();
+
   return (
-    <div>
-      CardsDiplayer Component
-    </div>
+    <ul>
+      {data.schools.map(school => (
+        <li key={school.id} className="cursor-pointer hover:scale-[1.009]">
+          <Link to={`/school/${school.id}`}>
+            <Card title={school.name.en || school.name.original} titleSecondary={school.name.original} description={addressFormatter(school.address)} />
+          </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
