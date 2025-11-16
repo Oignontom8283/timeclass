fa7c11f54540333086f3159a74114ed8b82b9243
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-11-17 00:15:06 +0100

    fix: Update Layout component structure for improved styling and layout consistency

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,15 +2,14 @@ import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen flex flex-col">
       <header className="w-full h-12 flex items-center justify-center border-b border-gray-300 bg-zinc-50">
         <div className='font-mono text-2xl font-bold'>TimeClass</div>
       </header>
 
-      <main className="">
+      <main className="flex flex-col grow">
         <Outlet />
       </main>
     </div>
   );
 };
-
