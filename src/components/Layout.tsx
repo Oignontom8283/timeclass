ea4e1afe1c6d803b4453e72df79e24706830941d
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-11-17 23:48:10 +0100

    feat: Wrap TimeClass title in a Link component for navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
       <header className="w-full h-12 flex items-center justify-center border-b border-gray-300 bg-zinc-50">
-        <div className='font-mono text-2xl font-bold'>TimeClass</div>
+        <Link to="/">
+          <div className='font-mono text-2xl font-bold'>TimeClass</div>
+        </Link>
       </header>
 
       <main className="flex flex-col grow">
