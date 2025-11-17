d5dd9f98218ad73e662d9b77a18b436ad2247d0c
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-11-17 13:58:47 +0100

    fix: Correct route path for School component to use schoolId parameter

--- src/main.tsx
+++ src/main.tsx
@@ -19,9 +19,9 @@ createRoot(document.getElementById('root')!).render(
             
             <Route element={<Layout />}>
               <Route path="/" element={<Home />} />
-              <Route path='school/:id' element={<School />} />
-              <Route path="*" element={<NotFound />} />
+              <Route path='school/:schoolId' element={<School />} />
             </Route>
+            <Route path="*" element={<NotFound />} />
             <Route path='school/:schoolId/timestamp/:timestampId' element={<Timestamp />} />
 
           </Routes>
