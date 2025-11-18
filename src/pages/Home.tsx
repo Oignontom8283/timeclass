8a8d5f6a5b3245d7553506cc8be29a787576891b
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-11-19 00:26:28 +0100

    fix: CEO

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,9 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center min-h-0 grow">
-      <div className="mt-24 flex flex-col items-center justify-center gap-2">
-        <h1 className="text-3xl">Welcome to <span className="font-bold font-mono">TimeClass</span> !</h1>
-        <sub className="text-2xl italic">Schedule for your school.</sub>
+      <div className="mt-24 flex flex-col items-center justify-center gap-4">
+        <sub className="text-3xl">Welcome to <span className="font-bold font-mono">TimeClass</span> !</sub>
+        <h1 className="text-2xl italic">Schedule for your school.</h1>
       </div>
       <div className="mt-10">
         <CardsDiplayer />
