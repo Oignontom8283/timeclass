a6878d242c6c7cbda33bc59c03606e88839d8266
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-11-18 21:20:55 +0100

    fix: Change <p> to <h1>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-0 grow">
       <div className="mt-24 flex flex-col items-center justify-center gap-2">
-        <p className="text-3xl">Welcome to <span className="font-bold font-mono">TimeClass</span> !</p>
-        <p className="text-2xl italic">Schedule for your school.</p>
+        <h1 className="text-3xl">Welcome to <span className="font-bold font-mono">TimeClass</span> !</h1>
+        <sub className="text-2xl italic">Schedule for your school.</sub>
       </div>
       <div className="mt-10">
         <CardsDiplayer />
