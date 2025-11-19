13ede83fe21c7b39818fba98793242456b757d7a
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-11-19 17:02:14 +0100

    feat: Implement text input with dynamic width and placeholder in Timestamp component

--- src/pages/Timestamp.tsx
+++ src/pages/Timestamp.tsx
@@ -6,7 +6,9 @@ import { useEffect, useState } from "react";
 
 export default function Timestamp() {
 
+  const [textDisplayed, setTextDisplayed] = useState("");
   const [fullScreenMode, setFullScreenMode] = useState(false);
+  const [inputWidth, setInputWidth] = useState(200);
 
   useEffect(() => {
     const handleFullscreenChange = () => {
@@ -89,8 +91,18 @@ export default function Timestamp() {
       </div>
       
       {/* text input diplayer */}
-      <div className={"fixed top-[37.5%] -translate-y-1/2 left-1/2 -translate-x-1/2 border-green-400 " + (fullScreenMode ? "border-0" : "border border-dotted")}> {/* 37.5% = 3/8 = (1/4 + 1/8) */}
-        {/* TODO: Text input here */}
+      <div className={"fixed top-[37.5%] -translate-y-1/2 left-1/2 -translate-x-1/2 " + (fullScreenMode ? "border-0" : "border-dotted border-2 border-zinc-300 rounded-3xl")}> {/* 37.5% = 3/8 = (1/4 + 1/8) */}
+        <span className="invisible absolute whitespace-pre text-xl p-4" ref={(span) => {span && setInputWidth(Math.max(span.offsetWidth, 200))}} >
+          {textDisplayed || "Taper votre text ici..."}
+        </span>
+        <input
+          type="text"
+          value={textDisplayed}
+          onChange={e => setTextDisplayed(e.target.value)}
+          className="bg-transparent border-0 outline-0 text-center text-xl p-4 text-black"
+          placeholder={fullScreenMode && textDisplayed.length === 0 ? "" : "Taper votre text ici..."}
+          style={{ width: `${inputWidth}px` }}
+        />
       </div>
 
     </div>
