840f5a3c0013185a990d0a8bacbdcb93658d6ad7
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-11-25 07:50:54 +0000

    refactor: simplify header structure in Layout component

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -4,13 +4,12 @@ export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
       <header className="w-full h-12 flex items-center justify-center border-b border-gray-300 bg-zinc-50">
-        <Link to="/">
-          <div className='flex flex-row items-center justify-center gap-2'>
-            <img src="/assets/logo.png" alt="logo" className='h-7 w-7'/>
-            <span className='font-mono text-2xl font-bold'>TimeClass</span>
-
-          </div>
+        
+        <Link to="/" className='flex flex-row items-center justify-center gap-2'>
+          <img src="/assets/logo.png" alt="logo" className='h-7 w-7'/>
+          <span className='font-mono text-2xl font-bold'>TimeClass</span>
         </Link>
+        
       </header>
 
       <main className="flex flex-col grow">
