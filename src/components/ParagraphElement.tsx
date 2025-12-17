d5d48afae05d94d6b2f023e3ecb57b14ea349072
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2025-12-18 00:35:15 +0100

    feat: enhance ParagraphElement with editable text area and styling

--- src/components/ParagraphElement.tsx
+++ src/components/ParagraphElement.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 
 
 export default function ParagraphElement({ value, onChange }: { value: string; onChange: (newValue: string) => void }) {
+
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
-    <div>
-      <p>{value}</p>
+    <div className={"p-1 font-sans w-full h-full " + (isEditing ? "bg-gray-500/90" : "")}>
+      {!isEditing ? (
+        <p
+          onClick={() => setIsEditing(true)}
+          className="cursor-text whitespace-pre-wrap m-0 p-0"
+        >
+          {value}
+        </p>
+      ) : (
+        <textarea
+          onChange={e => onChange(e.target.value)}
+          value={value}
+          className={"w-full h-full bg-transparent resize-none outline-none border-none whitespace-pre-wrap m-0 p-0" + (isEditing ? ' bg-white' : "")}
+          onBlur={() => setIsEditing(false)}
+          autoFocus
+        />
+      )}
     </div>
   )
 }
